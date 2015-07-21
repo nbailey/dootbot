c0bd611abfcb5e89d8e658f76803c3db8b511627
Nate Bailey <natekbailey@gmail.com>
2015-07-21 00:47:13 -0400

    v1.0
    Init.

---
+++ roboskeltal.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 22:15:46 2015
+
+@author: Nate
+"""
+
+import praw
+import random
+import time
+
+from datetime import datetime
+from templates import *
+
+
+user_agent = 'LeDootGeneration_SS reposter bot 1.0 by /u/slate15'
+
+print 'Starting RoboSkeltal! Doot doot.'
+
+print 'Signing in...'
+
+r = praw.Reddit(user_agent=user_agent)
+r.login('roboskeltal', 'CALc1um')
+
+print 'Signed in!'
+
+user_name = 'ledootgeneration_ss'
+user = r.get_redditor(user_name)
+
+submission_ids = [s.id for s in user.get_submitted(limit=None)]
+comment_ids = [c.id for c in user.get_comments(limit=None)]
+
+print 'Setup complete\n'
+
+while True:
+    # Get recent submissions
+    print 'Finding submissions...'
+    submissions = user.get_submitted(limit=5)
+    
+    # Repost new submissions to /r/ledootgeneration
+    for submission in submissions:
+        if submission.id not in submission_ids:
+            print 'New submission found!'
+            
+            title = submission.title
+            # Copy self text for self posts
+            if submission.is_self:
+                selftext = submission.selftext
+                s = r.submit('ledootgeneration', title=title, text=selftext)
+            # Copy url for link posts                
+            else:
+                url = submission.url
+                s = r.submit('ledootgeneration', title=title, url=url)
+            
+            print 'Submitted to /r/ledootgeneration: {}'.format(s.permalink)
+            
+            # Post to /r/roboskeltal linking to original and copy
+            dt = datetime.fromtimestamp(s.created_utc)
+            orig = submission.permalink
+            xpost = s.permalink
+            s2 = r.submit('roboskeltal', title=title,
+                          text=submission_template.format(dt, orig, xpost))
+            
+            print 'Submitted to /r/roboskeltal: {}'.format(s2.permalink)
+            
+            # Add to list of submission ids
+            submission_ids.append(submission.id)
+            
+    # Get recent comments
+    print 'Finding comments...'
+    comments = user.get_comments(limit=5)
+    
+    # Repost new comments on rising /r/ledootgeneration posts
+    for comment in comments:
+        if comment.id not in comment_ids:
+            print 'New comment found!'            
+            
+            # Find a /r/ledootgeneration post to comment on
+            rising = list(r.get_subreddit('ledootgeneration').get_rising())
+            thread = random.choice(rising)
+            
+            c = thread.add_comment(comment.body)
+            print 'Commented on /r/ledootgeneration: {}'.format(c.permalink)
+            
+            # Post to /r/roboskeltal linking to original and copy
+            dt = datetime.fromtimestamp(c.created_utc)
+            orig = comment.permalink
+            xpost = c.permalink
+            title = (comment.body[:297] + '...') if len(comment.body)>300 else comment.body
+            s = r.submit('roboskeltal', title=title,
+                     text=comment_template.format(dt, orig, xpost))
+                     
+            print 'Submitted to /r/roboskeltal: {}'.format(s.permalink)
+            
+            # Add to list of comment ids
+            comment_ids.append(comment.id)
+    
+    print 'Sleeping... sweet dreams of calcium and updoots'
+    time.sleep(1800)
---
+++ templates.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 00:43:47 2015
+
+@author: Nate
+"""
+
+submission_template = '''At {} UTC, /u/roboskeltal crossposted a submission from /r/subredditsimulator to /r/ledootgeneration.
+
+Click [here]({}) to see the /r/subredditsimulator submission.  
+Click [here]({}) to see the /r/ledootgeneration submission.
+
+Thank Mr Skeltal for good bones and calcium. Doot doot.'''
+
+comment_template = '''At {} UTC, /u/roboskeltal crossposted a comment from /r/subredditsimulator to /r/ledootgeneration.
+
+Click [here]({}) to see the /r/subredditsimulator comment.  
+Click [here]({}) to see the /r/ledootgeneration comment.
+
+Thank Mr Skeltal for good bones and calcium. Doot doot.'''
