dd1a11e95495d611ab32500742215673ef223b81
Nate Bailey <natebailey@tbp.berkeley.edu>
2015-07-21 16:35:49 -0700

    Small update to /r/roboskeltal template

--- templates.py
+++ templates.py
@@ -5,16 +5,16 @@ Created on Tue Jul 21 00:43:47 2015
 @author: Nate
 """
 
-submission_template = '''At {} UTC, /u/roboskeltal crossposted a submission from /r/subredditsimulator to /r/ledootgeneration.
+submission_template = '''At {} UTC, I crossposted a submission from /r/subredditsimulator to /r/ledootgeneration.
 
-Click [here]({}) to see the /r/subredditsimulator submission.  
+Click [here]({}) to see the /r/subredditsimulator submission.
 Click [here]({}) to see the /r/ledootgeneration submission.
 
-Thank Mr Skeltal for good bones and calcium. Doot doot.'''
+Thank Mr Skeltal for good circuits and silicon. Doot doot.'''
 
-comment_template = '''At {} UTC, /u/roboskeltal crossposted a comment from /r/subredditsimulator to /r/ledootgeneration.
+comment_template = '''At {} UTC, I crossposted a comment from /r/subredditsimulator to /r/ledootgeneration.
 
-Click [here]({}) to see the /r/subredditsimulator comment.  
+Click [here]({}) to see the /r/subredditsimulator comment.
 Click [here]({}) to see the /r/ledootgeneration comment.
 
-Thank Mr Skeltal for good bones and calcium. Doot doot.'''
+Thank Mr Skeltal for good circuits and silicon. Doot doot.'''
