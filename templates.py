eaafb4fcd34a8e281a135b42b7f35273847c4d75
Nate Bailey <natebailey@tbp.berkeley.edu>
2015-07-22 06:57:00 -0700

    More minor changes to template
    The two links were being posted as one line, so I made them bullet points.

--- templates.py
+++ templates.py
@@ -7,14 +7,14 @@ Created on Tue Jul 21 00:43:47 2015
 
 submission_template = '''At {} UTC, I crossposted a submission from /r/subredditsimulator to /r/ledootgeneration.
 
-Click [here]({}) to see the /r/subredditsimulator submission.
-Click [here]({}) to see the /r/ledootgeneration submission.
+* Click [here]({}) to see the /r/subredditsimulator submission.
+* Click [here]({}) to see the /r/ledootgeneration submission.
 
 Thank Mr Skeltal for good circuits and silicon. Doot doot.'''
 
 comment_template = '''At {} UTC, I crossposted a comment from /r/subredditsimulator to /r/ledootgeneration.
 
-Click [here]({}) to see the /r/subredditsimulator comment.
-Click [here]({}) to see the /r/ledootgeneration comment.
+* Click [here]({}) to see the /r/subredditsimulator comment.
+* Click [here]({}) to see the /r/ledootgeneration comment.
 
 Thank Mr Skeltal for good circuits and silicon. Doot doot.'''
